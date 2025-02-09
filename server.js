f5af617bc11207e59d3f29b3e0997e09d3fc1c36
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2025-02-09 19:57:10 +0530

    modifications

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express")
 const app = express()
 const transporter = require("./utils/transporter");
+const cors = require("cors")
 
 require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(cors({
+    origin: process.env.PORTFOLIO_URL ,
+    methods: ["POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}));
 
 const PORT = process.env.PORT || 5000
 
@@ -23,7 +33,7 @@ app.post("/send-otp", async(req, res) => {
         from: "shivangbhaiisgreat@gmail",
         to: req.body.email , 
         subject: "Mosshead | Shivang",
-        text: `Hello ${req.body.name}!, I have recieved you message from our Portfolio website that ${req.body.message}, I'd like to help you in create any project, so I'll create you within 2 or 3 working days. We are happy to work with you. Thank You for contacting us! `,
+        text: `Hello ${req.body.name}!, I have recieved you message from my Portfolio website, I'd like to help you in create any project, so I'll contact you within 2 or 3 working days. We are happy to work with you. For any Querries you can reply to this mail or contact me on any social media platform from my portfolio. Thank You for contacting us! `,
     } 
     
     try {
